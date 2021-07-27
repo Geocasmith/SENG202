917c6ff97993cabc8b85c9c51cfd8bc4a9d58bd4
Bede <bedesv@gmail.com>
2021-07-27 12:39:44 +1200

    Initialised Maven

---
+++ src/main/java/seng202/group10/App.java
@@ -0,0 +1,22 @@
+package seng202.group10;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello SENG202 Team 10" );
+
+        MyObject myObject = new MyObject("chair", 3);
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(myObject);
+
+        System.out.println("myObject = " + myObject);
+        System.out.println("myObject stringfyied = " + jsonString);
+    }
+}
---
+++ src/main/java/seng202/group10/MyObject.java
@@ -0,0 +1,12 @@
+package seng202.group10;
+
+public class MyObject {
+
+    private String description;
+    private int quantity;
+
+    public MyObject(String description, int quantity) {
+        this.description = description;
+        this.quantity = quantity;
+    }
+}
