2ab7143ffb6611e0f582e5ea2ece3e025d36feac
Bede Skinner-Vennell <bjs185@uclive.ac.nz>
2021-08-05 18:44:25 +1200

    Changed tests to use junit 5 because otherwise they wouldn't work with GitLab CI - Also have one failing test

--- src/test/java/seng202/group10/AppTest.java
+++ src/test/java/seng202/group10/AppTest.java
@@ -1,54 +1,46 @@
 package seng202.group10;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+
+//    /**
+//     * @return the suite of tests being tested
+//     */
+//    public static Test suite()
+//    {
+//        return new TestSuite( AppTest.class );
+//    }
 
     /**
      * Rigourous Test :-)
      */
-    @org.junit.Test
+    @Test
      public void testApp()
     {
-        assertTrue( true );
+        assertTrue(true);
     }
-    @org.junit.Test
+    @Test
     public void GeorgeTestApp()
     {
-        assertTrue( true );
+        assertTrue(true);
     }
-    @org.junit.Test
+    @Test
     public void JonathanTestApp()
     {
-        assertTrue( true );
+        assertTrue(true);
     }
-    @org.junit.Test
+    @Test
     public void BedeTestApp()
     {
-        fail();
+        assertTrue(false);
     }
 }
