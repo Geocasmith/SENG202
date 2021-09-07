e475a2043caacfe9e01c7378252c86b6bb6bb2af
Bede Skinner-Vennell <bjs185@uclive.ac.nz>
2021-09-07 21:08:06 +1200

    Changed the graphing software to group dates into ranges
    Cuts the time between first and last record into segments then adds up total number of crimes committed in each segment and graphs the segments

--- src/main/java/gui/Graph.java
+++ src/main/java/gui/Graph.java
@@ -2,11 +2,14 @@ package gui;
 
 import backend.Record;
 import backend.database.Database;
+import backend.DataAnalyser;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.chart.*;
 import javafx.stage.Stage;
 
+import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -15,6 +18,7 @@ import java.sql.SQLException;
 
 public class Graph extends Application {
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a", Locale.ENGLISH);
+    private DataAnalyser dataAnalyser = new DataAnalyser();
     @Override
     public void start(Stage stage) throws SQLException {
         Database db = new Database();
@@ -29,7 +33,7 @@ public class Graph extends Application {
         LineChart lineChart = createWardCrimesOverTimeGraph(db, hm, series);
 
 
-        Scene scene  = new Scene(barChart,800,600);
+        Scene scene  = new Scene(barChart,1920,1080);
         barChart.getData().add(series);
 
         stage.setScene(scene);
@@ -48,8 +52,8 @@ public class Graph extends Application {
 
         ArrayList<Record> data = new ArrayList<>();
         data = db.getAll();
-        List<LocalDateTime> times = new ArrayList<>();
-        for (Record r: data) {
+        ArrayList<LocalDateTime> times = new ArrayList<>();
+        for (Record r: data.subList(0, 50)) {
             times.add(r.getDateAsObject());
             if (hm.containsKey(r.getDateAsObject())) {
                 hm.put(r.getDateAsObject(), (int) hm.get(r.getDateAsObject()) + 1);
@@ -58,8 +62,32 @@ public class Graph extends Application {
             }
         }
         Collections.sort(times);
-        for (LocalDateTime t: times) {
-            series.getData().add(new XYChart.Data(t.format(formatter), hm.get(t)));
+
+        LocalDateTime lowerBound = times.get(0);
+        LocalDateTime upperBound;
+        int i = 0;
+        int count;
+        LinkedHashMap hm2 = new LinkedHashMap();
+        ArrayList<LocalDateTime> times2 = new ArrayList<>();
+        Duration width = dataAnalyser.calculateTimeDifference(times.get(0), times.get(times.size() - 1));
+        Duration segments = Duration.ofSeconds(width.getSeconds() / 5);
+        while (Duration.between(times.get(times.size() - 1), lowerBound).getSeconds() < 0) {
+            upperBound = lowerBound.plusSeconds(segments.getSeconds());
+            count = 0;
+            while (i < times.size() && Duration.between(times.get(i), upperBound).getSeconds() > 0) {
+                count += (int) hm.get(times.get(i));
+                i++;
+            }
+            times2.add(lowerBound);
+            hm2.put(lowerBound, count);
+            lowerBound = upperBound;
+        }
+        times2.add(lowerBound);
+        hm2.put(lowerBound, 0);
+
+        for (LocalDateTime t: times2) {
+            series.getData().add(new XYChart.Data(t.format(formatter), hm2.get(t)));
+            System.out.println(t.format(formatter));
         }
         return barChart;
     }
@@ -72,7 +100,7 @@ public class Graph extends Application {
         //creating the chart
         final LineChart lineChart = new LineChart(xAxis,yAxis);
 
-        lineChart.setTitle("Crimes by Time");
+        lineChart.setTitle("Crimes per Ward by Time");
 
         ArrayList<Record> data = new ArrayList<>();
         ArrayList<Integer> wards = new ArrayList<>();
@@ -90,9 +118,9 @@ public class Graph extends Application {
             }
         }
         Collections.sort(wards);
-        for (int i: wards) {
-            System.out.println(i);
-        }
+//        for (int i: wards) {
+//            System.out.println(i);
+//        }
         Collections.sort(times);
         for (LocalDateTime t: times) {
             series.getData().add(new XYChart.Data(t.format(formatter), hm.get(t)));
