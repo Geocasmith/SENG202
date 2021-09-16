b890d43bb8a5b5b6062860d6d721cef3ebec199d
george <carrsmithg@gmail.com>
2021-09-16 18:06:21 +1200

    Added tests for database filter (checks if it works with different values parsed in)

--- src/test/java/backend/database/DatabaseTest.java
+++ src/test/java/backend/database/DatabaseTest.java
@@ -1,10 +1,13 @@
 package backend.database;
 
+import backend.Record;
 import com.opencsv.exceptions.CsvValidationException;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 class DatabaseTest {
@@ -19,5 +22,47 @@ class DatabaseTest {
             System.out.println(extractedCol.get(i));
         }
 
+    }
+    @Test
+    void filterTwoDates() throws SQLException, ParseException {
+        Database d = new Database();
+        d.connectDatabase();
+        ArrayList<String> crimeTypes = new ArrayList<String>();
+        ArrayList<String> locationDescriptions = new ArrayList<String>();
+        ArrayList<Record>str = d.getFilter(new SimpleDateFormat("MM/dd/yyyy h:mm:ss a").parse("07/05/2020 12:00:00 PM"),new SimpleDateFormat("MM/dd/yyyy h:mm:ss a").parse("07/18/2020 08:00:00 AM"),crimeTypes,locationDescriptions,null,null,null,null,null,null);
+
+        int count = 1;
+        for (Record r:str){
+            System.out.println(count+":"+r.toString());
+            count ++;
+        }
+    }
+    @Test
+    void filterStartDate() throws SQLException, ParseException {
+        Database d = new Database();
+        d.connectDatabase();
+        ArrayList<String> crimeTypes = new ArrayList<String>();
+        ArrayList<String> locationDescriptions = new ArrayList<String>();
+        ArrayList<Record>str = d.getFilter(new SimpleDateFormat("MM/dd/yyyy h:mm:ss a").parse("07/05/2020 12:00:00 PM"),null,crimeTypes,locationDescriptions,null,null,null,null,null,null);
+
+        int count = 1;
+        for (Record r:str){
+            System.out.println(count+":"+r.toString());
+            count ++;
+        }
+    }
+    @Test
+    void filterEndDate() throws SQLException, ParseException {
+        Database d = new Database();
+        d.connectDatabase();
+        ArrayList<String> crimeTypes = new ArrayList<String>();
+        ArrayList<String> locationDescriptions = new ArrayList<String>();
+        ArrayList<Record>str = d.getFilter(null,null,crimeTypes,locationDescriptions,null,null,null,null,null,null);
+
+        int count = 1;
+        for (Record r:str){
+            System.out.println(count+":"+r.toString());
+            count ++;
+        }
     }
 }
