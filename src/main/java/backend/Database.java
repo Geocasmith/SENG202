1f05b8e3d5253201bb4fd481c95537d92703d789
Jonathan Tomlinson <jdt59@uclive.ac.nz>
2021-10-04 15:28:56 +1300

    Applying filters with no date range will now show all crimes regardless of date
    Previously they had to be after Jan 1st 1970 (unix time start date)

--- src/main/java/backend/Database.java
+++ src/main/java/backend/Database.java
@@ -518,7 +518,7 @@ public class Database {
      */
     public ArrayList<Record> getFilter(String caseNumber, Date startDate, Date endDate,ArrayList<String> crimeTypes,ArrayList<String> locDes,String ward,String beat,String lat,String lon,int radius,String arrest,String domestic) throws SQLException {
         connection.setAutoCommit(false);
-        String SQLString = "SELECT * FROM CRIMES where (UNIXTIME >= 0) ";
+        String SQLString = "SELECT * FROM CRIMES where true ";
 
         //Cycles through Crime Type values if not empty and appends to SQL string
         if(!crimeTypes.isEmpty()){
