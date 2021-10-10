0976189f22498a4b14912a1c58d7445c8de9f25b
ste73 (Sofonias) <ste73@uclive.ac.nz>
2021-10-10 23:33:49 +1300

    Added FrequencyComparatorAscendingTest

---
+++ src/test/java/unitTests/FrequencyComparatorAscendingTest.java
@@ -0,0 +1,54 @@
+package unitTests;
+
+import frequencyComparator.FrequencyComparatorAscending;
+import frequencyComparator.TypeFrequencyPair;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests FrequencyComparatorAscending class
+ * @author Sofonias Tekele Tesfaye
+ */
+
+class FrequencyComparatorAscendingTest {
+
+    @Test
+    void compare() {
+        TypeFrequencyPair testPair = new TypeFrequencyPair();
+        ArrayList<TypeFrequencyPair>  testList = new ArrayList<>();
+        ArrayList<TypeFrequencyPair>  comparisonList = new ArrayList<>();
+        int frequency = 100;
+
+        // Generate a TypeFrequency pair list sorted in descending frequency order
+        for (int i= 0; i <=4; i++) {
+            testPair.setType("Theft");
+            testPair.setFrequency(frequency);
+            testList.add(testPair);
+            frequency -= 20;
+
+        }
+
+        // Generate a TypeFrequency pair list sorted in ascending frequency order
+        frequency = 20;
+        for (int i= 0; i <=4; i++) {
+            testPair.setType("Theft");
+            testPair.setFrequency(frequency);
+            comparisonList.add(testPair);
+            frequency += 20;
+
+        }
+        //Sort testList in ascending order of frequency
+        testList.sort(new FrequencyComparatorAscending());
+
+      assertEquals(testList.get(0).getFrequency(), comparisonList.get(0).getFrequency());
+      assertEquals(testList.get(4).getFrequency(), comparisonList.get(4).getFrequency());
+
+
+
+
+
+    }
+}
